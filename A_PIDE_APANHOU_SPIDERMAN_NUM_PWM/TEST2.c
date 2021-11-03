e7cf4103722bdb79d4d2c5635d2af778eda1232f
Shaunted <sarasoaresh@gmail.com>
2021-11-03 19:55:05 +0000

    New file with 3 ADC readings + some PID

--- A_PIDE_APANHOU_SPIDERMAN_NUM_PWM/TEST2.c
+++ A_PIDE_APANHOU_SPIDERMAN_NUM_PWM/TEST2.c
@@ -76,7 +76,6 @@
 #include <stdbool.h>
 #include <string.h>
 
-
 // Function Defines~
 
 void PID_function(uint16_t sensor);
@@ -85,20 +84,18 @@ void PID_function(uint16_t sensor);
 
 #define UMAX    65535 // Around 3.3V
 #define UMIN    19858 // Around 1V
-#define DT      655
+#define DT      655200
 #define KP      100
 #define KI      25
 //#define IDEAL   512
 
-
 // SPI Variables
 
-uint8_t ReceiveBuffer[8] = {0};
+uint8_t ReceiveBuffer[8] = { 0 };
 uint8_t ReceiveIndex = 0;
 uint16_t ReceivedValue = 0;
-uint8_t TransmitBuffer[8] = {0};
+uint8_t TransmitBuffer[8] = { 0 };
 uint8_t TransmitIndex = 1;
-
 
 // ADC/PID Variables
 uint16_t ADC_Result;
@@ -108,150 +105,150 @@ int32_t error = 0;      // Q0
 int64_t u = 0;
 int64_t error_i = 0;
 
-
+bool ADC_FLAG = 0;
 
 int main(void)
 {
-  WDTCTL = WDTPW | WDTHOLD;                 // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop watchdog timer
 
-
-  // Configure PWM 1.6 pin
-  P1DIR |= BIT6;                  // P1.6 output
-  P1SEL0 &= ~BIT6;                 // P1.6 options select
-  P1SEL1 |= BIT6;
+    // Configure PWM 1.6 pin
+    P1DIR |= BIT6;                  // P1.6 output
+    P1SEL0 &= ~BIT6;                 // P1.6 options select
+    P1SEL1 |= BIT6;
 
 // Configure ADC A5 pin
-  P1SEL0 |= BIT5;
-  P1SEL1 |= BIT5;
-
-
+    P1SEL0 |= BIT5;
+    P1SEL1 |= BIT5;
 
 // CLOCK CONFIG
 
-  // Configure one FRAM waitstate as required by the device datasheet for MCLK
-  // operation beyond 8MHz _before_ configuring the clock system.
-  FRCTL0 = FRCTLPW | NWAITS_1;
+    // Configure one FRAM waitstate as required by the device datasheet for MCLK
+    // operation beyond 8MHz _before_ configuring the clock system.
+    FRCTL0 = FRCTLPW | NWAITS_1;
 
-  __bis_SR_register(SCG0);                           // disable FLL
-  CSCTL3 |= SELREF__REFOCLK;                         // Set REFO as FLL reference source
-  CSCTL0 = 0;                                        // clear DCO and MOD registers
-  CSCTL1 &= ~(DCORSEL_7);                            // Clear DCO frequency select bits first
-  CSCTL1 |= DCORSEL_5;                               // Set DCO = 16MHz
-  CSCTL2 = FLLD_0 + 487;                             // DCOCLKDIV = 16MHz
-  __delay_cycles(3);
-  __bic_SR_register(SCG0);                           // enable FLL
-  while(CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1));         // FLL locked
-
-
+    __bis_SR_register(SCG0);                           // disable FLL
+    CSCTL3 |= SELREF__REFOCLK;               // Set REFO as FLL reference source
+    CSCTL0 = 0;                                   // clear DCO and MOD registers
+    CSCTL1 &= ~(DCORSEL_7);             // Clear DCO frequency select bits first
+    CSCTL1 |= DCORSEL_5;                               // Set DCO = 16MHz
+    CSCTL2 = FLLD_0 + 487;                             // DCOCLKDIV = 16MHz
+    __delay_cycles(3);
+    __bic_SR_register(SCG0);                           // enable FLL
+    while (CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1));         // FLL locked
 
 // SPI CONFIG
 
-  P2SEL0 |= BIT2 | BIT3 | BIT4 | BIT5;      // set 4-SPI pin as second function
-  SYSCFG2 |= USCIBRMP;                      // eUSCI_B0 port remapped to P2.2-P2.5
+    P2SEL0 |= BIT2 | BIT3 | BIT4 | BIT5;     // set 4-SPI pin as second function
+    SYSCFG2 |= USCIBRMP;                  // eUSCI_B0 port remapped to P2.2-P2.5
 
-  UCB0CTLW0 |= UCSWRST;                     // **Put state machine in reset**
-                                            // 4-pin, 8-bit SPI slave
-  UCB0CTLW0 = UCSYNC|UCMSB|UCMODE_2|UCSTEM|UCCKPH;
-                                                  // MSB
-  UCB0CTLW0 &= ~UCSSEL;
-  UCB0CTLW0 |= UCSSEL__SMCLK;               // Select SMCLK
-  UCB0BR0 = 0x02;                           // BRCLK = SMCLK/2
-  UCB0BR1 = 0;                              //
-  UCB0CTLW0 &= ~UCSWRST;                    // **Initialize USCI state machine**
-  UCB0IE |= UCRXIE;                         // Enable USCI_B0 RX interrupt
+    UCB0CTLW0 |= UCSWRST;                     // **Put state machine in reset**
+                                              // 4-pin, 8-bit SPI slave
+    UCB0CTLW0 = UCSYNC | UCMSB | UCMODE_2 | UCSTEM | UCCKPH;
+    // MSB
+    UCB0CTLW0 &= ~UCSSEL;
+    UCB0CTLW0 |= UCSSEL__SMCLK;               // Select SMCLK
+    UCB0BR0 = 0x02;                           // BRCLK = SMCLK/2
+    UCB0BR1 = 0;                              //
+    UCB0CTLW0 &= ~UCSWRST;                  // **Initialize USCI state machine**
+    UCB0IE |= UCRXIE;                         // Enable USCI_B0 RX interrupt
 
+    PM5CTL0 &= ~LOCKLPM5; // Disable the GPIO power-on default high-impedance mode
+                          // to activate previously configured port settings
 
-  PM5CTL0 &= ~LOCKLPM5;                     // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
+    // Configure ADC10
+    ADCCTL0 |= ADCMSC | ADCON;                         // ADCON, S&H=16 ADC clks
+    ADCCTL1 |= ADCSHP | ADCSHS_2 | ADCSSEL_2 | ADCCONSEQ_2; // ADCCLK = MODOSC; sampling timer
+    ADCCTL2 |= ADCRES;                              // 10-bit conversion results
+    ADCMCTL0 |= ADCINCH_5 | ADCSREF_1;         // A5 ADC input select; Vref=1.5V
+    ADCIE |= ADCIE0;
 
-  // Configure ADC10
-  ADCCTL0 |= ADCMSC | ADCON;                              // ADCON, S&H=16 ADC clks
-  ADCCTL1 |= ADCSHP | ADCSHS_2 | ADCSSEL_2 | ADCCONSEQ_2;             // ADCCLK = MODOSC; sampling timer
-  ADCCTL2 |= ADCRES;                                        // 10-bit conversion results
-  ADCMCTL0 |= ADCINCH_5 | ADCSREF_1;                        // A5 ADC input select; Vref=1.5V
-  ADCIE |= ADCIE0;
+    // Configure reference
+    PMMCTL0_H = PMMPW_H;                             // Unlock the PMM registers
+    PMMCTL2 |= INTREFEN;                            // Enable internal reference
+    __delay_cycles(400);                         // Delay for reference settling
 
+    // ADC Timer CONFIG (TB1)
+    //    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;        // SMCLK, up mode, clear TBR
 
-  // Configure reference
-  PMMCTL0_H = PMMPW_H;                                      // Unlock the PMM registers
-  PMMCTL2 |= INTREFEN;                                      // Enable internal reference
-  __delay_cycles(400);                                      // Delay for reference settling
+    //  TB1CCTL0 = CLLD_1;
+    TB1CCTL1 = OUTMOD_3;                            // CCR1 reset/set
+    TB1CCR0 = 10000;                              // PWM Period -> around 200 Hz
+    TB1CCR1 = 5000;
+    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR | CNTL__16 | ID_3; // SMCLK/8, up mode
 
-  // ADC Timer CONFIG (TB1)
-        //    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;        // SMCLK, up mode, clear TBR
+    // PWM Config (TB0)
+    TB0CCTL1 = OUTMOD_7;                            // CCR1 reset/set
+    TB0CCR1 = UMAX * 0;
+    TB0CTL = TBSSEL__SMCLK | MC__CONTINUOUS | TBCLR | CNTL__16;
 
-      //  TB1CCTL0 = CLLD_1;
-      TB1CCTL1 = OUTMOD_3;                            // CCR1 reset/set
-      TB1CCR0 = 32788;                                // PWM Period -> around 61 Hz
-      TB1CCR1 = 16394;
-      TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR | CNTL__16 | ID_3; // SMCLK/8, up mode
+    ADCCTL0 |= ADCENC;                          // Sampling and conversion start
 
+//    __bis_SR_register(LPM0_bits | GIE);
 
-        // PWM Config (TB0)
-      TB0CCTL1 = OUTMOD_7;                            // CCR1 reset/set
-      TB0CCR1 = UMAX*0;
-      TB0CTL = TBSSEL__SMCLK | MC__CONTINUOUS | TBCLR | CNTL__16;
+    while (1)
+    {
 
-      ADCCTL0 |= ADCENC;                            // Sampling and conversion start
+        PID_function(ADC_Result);
+        memcpy(&TransmitBuffer[6], &ADC_Result, 2);
+        //          TransmitBuffer[6] = 255;
+        if (u >= 0)
+        {
+            if (u > UMAX)
+            {
+                TB0CCR1 = UMAX;
+                memset(&TransmitBuffer[4], 0xFF, 2);
+            }
+            else
+            {
+                if (ideal == 0)
+                {
+                    TB0CCR1 = 0;
+                    uint16_t u_temp = 0;
 
-      __bis_SR_register(LPM0_bits | GIE);
+                    memcpy(&TransmitBuffer[4], &u_temp, sizeof(u_temp));
+                }
+                else
+                {
+                    TB0CCR1 = u;
+                    uint16_t u_temp = u;
 
-      while(1)
-      {
-          PID_function(ADC_Result);
-          memcpy(&TransmitBuffer[6], &ADC_Result, 2);
-//          TransmitBuffer[6] = 255;
-          if(u>=0){
-              if (u>UMAX){
-                  TB0CCR1 = UMAX;
-                  memset(&TransmitBuffer[4], 0xFF, 2);
-              }
-              else{
-                  if(ideal == 0){
-                      TB0CCR1 = 0;
-                      uint16_t u_temp = 0;
+                    memcpy(&TransmitBuffer[4], &u_temp, sizeof(u_temp));
+                }
+            }
+        }
+        else
+        {
+            TB0CCR1 = 0;
+            memset(&TransmitBuffer[4], 0, 2);
+        }
+        //          __delay_cycles(160000);
 
-                      memcpy(&TransmitBuffer[4], &u_temp, sizeof(u_temp));
-                  }
-                  else{
-                  TB0CCR1 = u;
-                  uint16_t u_temp = u;
-
-                  memcpy(&TransmitBuffer[4], &u_temp, sizeof(u_temp));
-                  }
-              }
-          }
-          else{
-              TB0CCR1 = 0;
-              memset(&TransmitBuffer[4], 0, 2);
-          }
-//          __delay_cycles(160000);
-      }
-    }
-
-// Functions Definitions
-
-void PID_function(uint16_t sensor){
-    // Control Logic
-    error = ideal;
-    error = error - sensor;
-    u = KP*error;                // Q0 = Q0*Q0
-    u = (u<<16) + KI*error_i;            // Q16 = Q16 + Q16
-    u = u>>16;
-
-
-    // Anti-windup
-    if((u >= UMAX || u < 0) && (((error >= 0) && (error_i >= 0)) || ((error < 0) && (error_i < 0)))){
-        error_i = error_i;
-    }
-    else{
-        error_i = error_i + DT*error;  // Q16
     }
 }
 
+// Functions Definitions
 
+void PID_function(uint16_t sensor)
+{
+    // Control Logic
+    error = ideal;
+    error = error - sensor;
+    u = KP * error;                // Q0 = Q0*Q0
+    u = (u << 16) + KI * error_i;            // Q16 = Q16 + Q16
+    u = u >> 16;
 
-
+    // Anti-windup
+    if ((u >= UMAX || u < 0)
+            && (((error >= 0) && (error_i >= 0))
+                    || ((error < 0) && (error_i < 0))))
+    {
+        error_i = error_i;
+    }
+    else
+    {
+        error_i = error_i + DT * error;  // Q16
+    }
+}
 
 // INTERRUPTS
 
@@ -271,24 +268,28 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
 
     ReceiveBuffer[ReceiveIndex++] = ucb0_rx_val;
 
-    switch(ReceiveBuffer[0]){
-        case 0xFF:
-            if(TransmitIndex < sizeof(TransmitBuffer)){
-                UCB0TXBUF = TransmitBuffer[TransmitIndex++];
+    switch (ReceiveBuffer[0])
+    {
+    case 0xFF:
+        if (TransmitIndex < sizeof(TransmitBuffer))
+        {
+            UCB0TXBUF = TransmitBuffer[TransmitIndex++];
 
-            }
-            break;
+        }
+        break;
 
-        case 0x00:
-            if(ReceiveIndex > 2){
-                ReceivedValue = ((ReceiveBuffer[2]<<8)|ReceiveBuffer[3]);
-                ideal = ReceivedValue;
-                UCB0TXBUF = ideal;
-            }
-            break;
+    case 0x00:
+        if (ReceiveIndex > 2)
+        {
+            ReceivedValue = ((ReceiveBuffer[2] << 8) | ReceiveBuffer[3]);
+            ideal = ReceivedValue;
+            UCB0TXBUF = ideal;
+        }
+        break;
     }
 
-    if(ReceiveIndex == sizeof(ReceiveBuffer)){
+    if (ReceiveIndex == sizeof(ReceiveBuffer))
+    {
         ReceiveIndex = 0;
         TransmitIndex = 1;
 //        __bic_SR_register_on_exit(LPM0_bits);              // Clear CPUOFF bit from LPM0
@@ -305,28 +306,27 @@ void __attribute__ ((interrupt(ADC_VECTOR))) ADC_ISR (void)
 #error Compiler not supported!
 #endif
 {
-    switch(__even_in_range(ADCIV,ADCIV_ADCIFG))
+    switch (__even_in_range(ADCIV, ADCIV_ADCIFG))
     {
-        case ADCIV_NONE:
-            break;
-        case ADCIV_ADCOVIFG:
-            break;
-        case ADCIV_ADCTOVIFG:
-            break;
-        case ADCIV_ADCHIIFG:
-            break;
-        case ADCIV_ADCLOIFG:
-            break;
-        case ADCIV_ADCINIFG:
-            break;
-        case ADCIV_ADCIFG:
-            ADC_Result = ADCMEM0;
-            __bic_SR_register_on_exit(LPM0_bits);              // Clear CPUOFF bit from LPM0
-            break;
-        default:
-            break;
+    case ADCIV_NONE:
+        break;
+    case ADCIV_ADCOVIFG:
+        break;
+    case ADCIV_ADCTOVIFG:
+        break;
+    case ADCIV_ADCHIIFG:
+        break;
+    case ADCIV_ADCLOIFG:
+        break;
+    case ADCIV_ADCINIFG:
+        break;
+    case ADCIV_ADCIFG:
+        ADC_Result = ADCMEM0;
+        ADC_FLAG = 1;
+        __bic_SR_register_on_exit(LPM0_bits);      // Clear CPUOFF bit from LPM0
+        break;
+    default:
+        break;
     }
 }
-
-
 
